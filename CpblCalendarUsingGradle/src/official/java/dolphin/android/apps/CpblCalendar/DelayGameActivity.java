24ab5000e0e631a6a0f45abe13f402581da11a7d
DolphinWing <dolphinwing74+github@gmail.com>
2015-03-07 18:14:01 +0800

    add download test codes
    	modified:   CpblCalendarUsingGradle/src/official/java/dolphin/android/apps/CpblCalendar/DelayGameActivity.java

--- CpblCalendarUsingGradle/src/official/java/dolphin/android/apps/CpblCalendar/DelayGameActivity.java
+++ CpblCalendarUsingGradle/src/official/java/dolphin/android/apps/CpblCalendar/DelayGameActivity.java
@@ -18,6 +18,9 @@ import java.util.regex.Pattern;
 import dolphin.android.apps.CpblCalendar.provider.AspNetHelper;
 import dolphin.android.apps.CpblCalendar.provider.CpblCalendarHelper;
 import dolphin.android.apps.CpblCalendar.provider.Game;
+import dolphin.android.net.GoogleDriveHelper;
+import dolphin.android.net.HttpProgressListener;
+
 
 public class DelayGameActivity extends Activity {
     private final static String TAG = "DelayGame";
@@ -35,6 +38,14 @@ public class DelayGameActivity extends Activity {
             public void onClick(View view) {
                 SparseArray<Game> games = queryDelayGame();
                 Log.d(TAG, "total delay games: " + games.size());
+            }
+        });
+
+        View button2 = findViewById(android.R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadFile();
             }
         });
     }
@@ -110,4 +121,46 @@ public class DelayGameActivity extends Activity {
         Log.v(TAG, String.format("delay game query wasted %d ms", ((endTime - startTime))));
         return delayedGames;
     }
+
+    private void downloadFile() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doDownloadFile();
+            }
+        }).start();
+    }
+
+    private void doDownloadFile() {
+        Log.d(TAG, "start download...");
+        File f = new File(getCacheDir(), "test.ogg");
+        //https://drive.google.com/file/d/0B-oMP4622t0hbFlrZTlpaXN4SUk/view?usp=sharing
+        boolean r = GoogleDriveHelper.download(this, "0B-oMP4622t0hbFlrZTlpaXN4SUk", f, mListener);
+        if (r) {
+            Log.d(TAG, f.getAbsolutePath());
+        } else {
+            Log.e(TAG, "gg!");
+        }
+    }
+
+    private HttpProgressListener mListener = new HttpProgressListener() {
+        private long startTime;
+
+        @Override
+        public void onStart(long contentSize) {
+            startTime = System.currentTimeMillis();
+            Log.d(TAG, String.format("onStart: size = %d", contentSize));
+        }
+
+        @Override
+        public void onUpdate(int bytes, long totalBytes) {
+            Log.d(TAG, String.format("onUpdate: byte = %d, total = %d", bytes, totalBytes));
+        }
+
+        @Override
+        public void onComplete(long totalBytes) {
+            Log.d(TAG, String.format("onComplete: size = %d, cost %d ms", totalBytes,
+                    System.currentTimeMillis() - startTime));
+        }
+    };
 }
