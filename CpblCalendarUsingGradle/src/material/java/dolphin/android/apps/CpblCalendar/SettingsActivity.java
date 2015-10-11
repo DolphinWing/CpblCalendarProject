76d2ddf8b0ffd872509c64d1c1b1b1fa4b7d18ab
DolphinWing <dolphinwing74+github@gmail.com>
2015-10-11 18:26:06 +0800

    fix 5.0+ Settings Toolbar color
    	modified:   CpblCalendarUsingGradle/src/material/java/dolphin/android/apps/CpblCalendar/SettingsActivity.java
    	modified:   CpblCalendarUsingGradle/src/material/res/layout/settings_toolbar.xml

--- CpblCalendarUsingGradle/src/material/java/dolphin/android/apps/CpblCalendar/SettingsActivity.java
+++ CpblCalendarUsingGradle/src/material/java/dolphin/android/apps/CpblCalendar/SettingsActivity.java
@@ -1,9 +1,17 @@
 package dolphin.android.apps.CpblCalendar;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -46,5 +54,34 @@ public class SettingsActivity extends PreferenceActivity {
                 finish();
             }
         });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        // Allow super to try and create a view first
+        final View result = super.onCreateView(name, context, attrs);
+        if (result != null) {
+            return result;
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            // If we're running pre-L, we need to 'inject' our tint aware Views in place of the
+            // standard framework versions
+            switch (name) {
+                case "EditText":
+                    return new AppCompatEditText(this, attrs);
+                case "Spinner":
+                    return new AppCompatSpinner(this, attrs);
+                case "CheckBox":
+                    return new AppCompatCheckBox(this, attrs);
+                case "RadioButton":
+                    return new AppCompatRadioButton(this, attrs);
+                case "CheckedTextView":
+                    return new AppCompatCheckedTextView(this, attrs);
+            }
+        }
+
+        return null;
     }
 }
